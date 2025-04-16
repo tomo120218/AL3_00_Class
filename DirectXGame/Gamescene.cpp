ef87a37cbb3365548bd758561c443180ae0221ee
tomohiro byoudou <k024g0017@g.neec.ac.jp>
2025-04-16 12:49:40 +0900

    絵いれた

--- DirectXGame/Gamescene.cpp
+++ DirectXGame/Gamescene.cpp
@@ -1,9 +1,22 @@
 #include "Gamescene.h"
 using namespace KamataEngine;
 
+GameScene::~GameScene() { 
+	delete model_; 
+}
+
 void GameScene::Initialize() {
+	// ファイル名を指定してテキスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
 
+	// 3Dモデルの生成
+	model_ = Model::Create();
 
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
+	// カメラの初期化
+	camera_.Initialize();
 }
 
 void GameScene::Update() {
@@ -12,6 +25,16 @@ void GameScene::Update() {
 }
 
 void GameScene::Draw() {
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
+	// 3Dモデル描画前処理
+	Model::PreDraw(dxCommon->GetCommandList());
 
+	// 3Dモデル
+	model_->Draw(worldTransform_, camera_, textureHandle_);
+	
+
+	// eDモデル描画後処理
+	Model::PostDraw();
 }
--- DirectXGame/Gamescene.h
+++ DirectXGame/Gamescene.h
@@ -5,6 +5,21 @@
 class GameScene {
 
 	public:
+
+		// テキスチャハンドル
+	    uint32_t textureHandle_ = 0;
+
+		// 3Dモデル
+	    KamataEngine::Model* model_ = nullptr;
+
+		// ワールドトランスフォーム
+	    KamataEngine::WorldTransform worldTransform_;
+
+		// カメラ
+	    KamataEngine::Camera camera_;
+
+		~GameScene();
+
 		// 初期化
 	    void Initialize();
 
